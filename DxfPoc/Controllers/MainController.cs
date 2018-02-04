fa607eb64c36753c76bfceb900d98f1d7478a5f6
Dmitriy Paskov <paskov.dmitriy@gmail.com>
2018-02-04 23:15:00 +0200

    Initial commit

---
+++ DxfPoc/Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using netDxf;
+using Newtonsoft.Json;
+
+namespace DxfPoc.Controllers
+{
+    [Produces("application/json")]
+    [Route("api")]
+    public class MainController : Controller
+    {
+        [HttpGet("test")]
+        public JsonResult Test()
+        {
+            var dxf1 = DxfDocument.Load(@"Drawings\Коллектор.dxf");
+            var dxf2 = DxfDocument.Load(@"Drawings\Втулки.dxf");
+            var dxf3 = DxfDocument.Load(@"Drawings\Детали для изг.dxf");
+
+            return new JsonResult(new
+            {
+                Circles = dxf2.Circles,
+                Lines = dxf2.Lines
+            },
+            new JsonSerializerSettings
+            {
+                ContractResolver = new ShouldSerializeContractResolver()
+            });
+        }
+    }
+}
---
+++ DxfPoc/ShouldSerializeContractResolver.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+namespace DxfPoc
+{
+    public class ShouldSerializeContractResolver : DefaultContractResolver
+    {
+        public new static readonly ShouldSerializeContractResolver Instance =
+                                      new ShouldSerializeContractResolver();
+
+        protected override JsonProperty CreateProperty(MemberInfo member,
+                                         MemberSerialization memberSerialization)
+        {
+            JsonProperty property = base.CreateProperty(member, memberSerialization);
+
+            if (property.PropertyName == "Owner"
+                || property.PropertyName == "Reactors")
+            {
+                property.ShouldSerialize = instance =>
+                {
+                    return false;
+                };
+            }
+
+            return property;
+        }
+    }
+}
